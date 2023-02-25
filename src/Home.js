336a440d5550cbc0734f14a801489e88bb1a8798
Rishitha N <nrishitha58@gmail.com>
2023-02-25 18:42:47 +0530

    Initial commit by Rishitha on 25/02/23 after creating all the components

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+    return (
+        <div className="home">
+
+            <div className="content">
+
+                <h1>Homepage</h1>
+
+            </div>
+
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ src/NavBar.js
@@ -0,0 +1,22 @@
+const NavBar = () => {
+    return (
+
+        <nav className="navbar">
+
+            <h1>Hello, this is navbar</h1>
+
+            <div className="links">
+
+                <a href="/">Home</a>
+                <a href="/login">Login</a>
+                <a href="/gallery">Gallery</a>
+                <a href="/contact">Contact</a>
+
+            </div>
+
+        </nav>
+        
+    );
+}
+ 
+export default NavBar;
